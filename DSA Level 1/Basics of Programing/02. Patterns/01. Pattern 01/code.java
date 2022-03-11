ce5e6f9c3f1ebfb2986d7e3c0db9dc6d0f20eff5
chirag <66241836+chiragbagri31@users.noreply.github.com>
2022-03-11 11:43:10 +0530

    Create code.java

---
+++ DSA Level 1/Basics of Programing/02. Patterns/01. Pattern 01/code.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=1; i<=n; i++)
+        {
+            // For stars
+            for(int k=1; k<=i; k++)
+            {
+                System.out.print("*\t");
+            }
+
+            // For spaces
+            for(int j=1; j<=n; j++)
+            {
+                System.out.print("\t");
+            }
+           
+            System.out.println();
+        }
+    }  
+}
